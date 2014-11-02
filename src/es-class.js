cce789d5c6a8d4880e23fa832b718f9bdff67f5c
Andrea Giammarchi <agiammarchi@twitter.com>
2014-11-02 12:02:16 +0000

    version 0.1.0

--- src/es-class.js
+++ src/es-class.js
@@ -1,1 +1,119 @@
-{}
+var Class = Class || (function (Object) {
+  'use strict';
+
+  /*! (C) Andrea Giammarchi - MIT Style License */
+
+  var
+    // shortcuts for minifiers and ES3 private keywords too
+    CONSTRUCTOR = 'constructor',
+    EXTENDS = 'extends',
+    PROTOTYPE = 'prototype',
+    STATIC = 'static',
+
+    // IE only bug
+    // used to copy non enumerable properties
+    nonEnumerables = [
+      CONSTRUCTOR,
+      'hasOwnProperty',
+      'isPrototypeOf',
+      'propertyIsEnumerable',
+      'toLocaleString',
+      'toString',
+      'valueOf'
+    ],
+
+    hasEnumerableBug = !{valueOf:0}[nonEnumerables[3]](nonEnumerables[6]),
+
+    hOP = Object[nonEnumerables[1]],
+
+    // basic ad-hoc private fallback for old browsers
+    // use es5-shim if you want a properly patched Object.create polyfill
+    create = Object.create || function (proto, descriptors) {
+      /*jshint newcap: false */
+      return (create[PROTOTYPE] = proto) ?
+        new create(null, descriptors) :
+        (this[CONSTRUCTOR] = descriptors[CONSTRUCTOR].value) && this
+      ;
+    },
+    defineProperty = Object.defineProperty
+  ;
+
+  // verified broken IE8
+  try {
+    defineProperty({}, '{}', {});
+  } catch(o_O) {
+    defineProperty = function (object, name, descriptor) {
+      object[name] = descriptor.value;
+    };
+  }
+
+  // copy enumerable source properties as tearget properties
+  // these might be copied as enumerable (i.e. statics) or not
+  function copyEnumerables(source, target, makeEnumerable) {
+    var key, i;
+    for (key in source) {
+      if (hOP.call(source, key)) {
+        setProperty(target, key, source[key], makeEnumerable);
+      }
+    }
+    if (hasEnumerableBug) {
+      for (i = 0; i < nonEnumerables.length; i++) {
+        key = nonEnumerables[i];
+        if (hOP.call(source, key)) {
+          setProperty(target, key, source[key], makeEnumerable);
+        }
+      }
+    }
+  }
+
+  // set a property via defineProperty using a common descriptor
+  function setProperty(target, key, value, makeEnumerable) {
+    defineProperty(target, key, {
+      enumerable: makeEnumerable,
+      configurable: true,
+      writable: true,
+      value: value
+    });
+  }
+
+  // Class({ ... })
+  function Class(description) {
+    var
+      constructor = hOP.call(description, CONSTRUCTOR) ?
+        description[CONSTRUCTOR] : function Class() {},
+      hasParent = hOP.call(description, EXTENDS),
+      parent = hasParent && description[EXTENDS],
+      inherits = hasParent && typeof parent === 'function' ?
+        parent[PROTOTYPE] : parent,
+      prototype = hasParent ?
+        create(inherits, {
+          constructor: {
+            configurable: true,
+            writable: true,
+            value: constructor
+          }
+        }) :
+        constructor[PROTOTYPE]
+    ;
+    if (hasParent) {
+      // in case it's a function
+      if (parent !== inherits) {
+        // copy possibly inherited statics
+        copyEnumerables(parent, constructor, true);
+      }
+      constructor[PROTOTYPE] = prototype;
+      delete description[EXTENDS];
+    }
+    if (hOP.call(description, STATIC)) {
+      // add new statics
+      copyEnumerables(description[STATIC], constructor, true);
+      delete description[STATIC];
+    }
+    // enrich the prototype
+    copyEnumerables(description, prototype, false);
+    return constructor;
+  }
+
+  return Class;
+
+}(Object));
--- test/es-class.js
+++ test/es-class.js
@@ -1,13 +1,94 @@
 //remove:
-var main = require('../build/es-class.node.js');
+var Class = require('../build/es-class.node.js');
 //:remove
 
 wru.test([
   {
     name: "main",
     test: function () {
-      wru.assert(typeof main == "object");
-      // wru.assert(0);
+      wru.assert(typeof Class == "function");
+    }
+  }, {
+    name: 'constructor',
+    test: function () {
+      var A = Class({
+        constructor: function A() {
+          wru.assert(this.constructor === A);
+        }
+      });
+      var B = Class({
+        'extends': A,
+        constructor: function () {
+          wru.assert(this.constructor === B);
+        }
+      });
+      var C = Class({});
+      new A;
+      new B;
+      wru.assert((new C).constructor === C);
+    }
+  }, {
+    name: 'extends',
+    test: function () {
+      var A = Class({});
+      var B = Class({
+        'extends': A
+      });
+      var C = Class({
+        'extends': B
+      });
+      wru.assert(new A instanceof A);
+      wru.assert(!(new A instanceof B));
+      wru.assert(!(new A instanceof C));
+      wru.assert(new B instanceof A);
+      wru.assert(new B instanceof B);
+      wru.assert(!(new B instanceof C));
+      wru.assert(new C instanceof A);
+      wru.assert(new C instanceof B);
+      wru.assert(new C instanceof C);
+    }
+  }, {
+    name: 'static',
+    test: function () {
+      var A = Class({
+        'static': {
+          A: 1
+        }
+      });
+      var B = Class({
+        'extends': A,
+        'static': {
+          B: 2
+        }
+      });
+      wru.assert(A.A === 1);
+      wru.assert(A.B === undefined);
+      wru.assert(A.hasOwnProperty('A'));
+      wru.assert(!A.hasOwnProperty('B'));
+      wru.assert(!A.prototype.hasOwnProperty('A'));
+      wru.assert(!A.prototype.hasOwnProperty('static'));
+      wru.assert(B.A === 1);
+      wru.assert(B.B === 2);
+      wru.assert(B.hasOwnProperty('A'));
+      wru.assert(B.hasOwnProperty('B'));
+      wru.assert(!B.prototype.hasOwnProperty('A'));
+      wru.assert(!B.prototype.hasOwnProperty('B'));
+      wru.assert(!B.prototype.hasOwnProperty('static'));
+      wru.assert(!B.prototype.hasOwnProperty('extends'));
+      wru.assert(new A instanceof A);
+      wru.assert(!(new A instanceof B));
+      wru.assert(new B instanceof A);
+      wru.assert(new B instanceof B);
+    }
+  }, {
+    name: 'probably non enumerable',
+    test: function () {
+      var A = Class({
+        toString: function () {
+          return 'a';
+        }
+      });
+      wru.assert(('' + new A) === 'a');
     }
   }
 ]);
